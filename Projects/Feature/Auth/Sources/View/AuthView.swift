fe06df91e832d4e73b5a98849587e72698685c06
2o1da <2o1daaaaaaa@naver.com>
2023-11-10 16:58:12 +0900

    dependencies를 추가합니다.

--- Projects/Feature/Auth/Sources/View/AuthView.swift
+++ Projects/Feature/Auth/Sources/View/AuthView.swift
@@ -7,15 +7,37 @@
 //
 
 import SwiftUI
+//import App
 
 public struct AuthView: View {
+    
+    let db = FirestoreManager.shared.db
     
     public init() {
         // Your initializations here
     }
     
     public var body: some View {
-        Text("This is Auth!")
+        HStack{
+            Text("This is Auth")
+            Button("Load Songs", action: loadSongs)
+        }
+    }
+    
+    private func loadSongs() {
+        db.collection("favorite").document("musicIDs").getDocument { (document, error) in
+            if let error = error {
+                print("Error getting document: \(error)")
+            } else if let document = document, document.exists {
+                if let musicIDs = document.data()?["IDs"] as? [String] {
+                    print("Music IDs: \(musicIDs)")
+                } else {
+                    print("No Music IDs")
+                }
+            } else {
+                print("Document does not exist")
+            }
+        }
     }
 }
 
---
+++ Tuist/Dependencies.swift
@@ -0,0 +1,17 @@
+//
+//  Dependencies.swift
+//  Config
+//
+//  Created by 다솔 on 2023/11/08.
+//  
+
+
+import ProjectDescription
+
+let dependencie = Dependencies(
+    swiftPackageManager: [
+        .remote(url: "https://github.com/firebase/firebase-ios-sdk.git", requirement: .upToNextMajor(from: "10.0.0")),
+        .remote(url: "https://github.com/Moya/Moya.git", requirement: .upToNextMajor(from: "15.0.0")),
+    ],
+    platforms: [.iOS]
+)
