856f6c5acdaba824dce429d5c751cfb0a4628fe2
kane04036 <hi011004@naver.com>
2024-02-03 20:30:24 +0900

    apple music genre check

--- Projects/Feature/Auth/Sources/View/SplashAndOnBoarding/SplashView.swift
+++ Projects/Feature/Auth/Sources/View/SplashAndOnBoarding/SplashView.swift
@@ -30,7 +30,7 @@ public struct SplashView: View {
             .navigationDestination(isPresented: $isNextViewPresenting) {
                 if isNextViewPresenting{
                     if hasUid ?? false {
-                        LoginView()
+                        MusickitTestView()
                     }else if hasLoginHistory ?? false {
                         LoginView()
                     }else {
--- Projects/Feature/Library/Sources/View/MusickitTestView.swift
+++ Projects/Feature/Library/Sources/View/MusickitTestView.swift
@@ -12,15 +12,43 @@ import MusicKit
 struct MusickitTestView: View {
     var body: some View {
         Button {
-            MusicTest()
+            genreCheck()
         } label: {
             Text("Button")
         }
 
     }
     
-    private func MusicTest(){
-        
+    private func genreCheck(){
+        let genreRequest = MusicCatalogResourceRequest<Genre>(matching: \.id, equalTo: "1153")
+//        genreRequest.
+//        MusicCatalogResourceRequest(matching: , equalTo: Value)
+//        genreRequest.
+        Task{
+            let response = try await genreRequest.response()
+            print(response.items)
+        }
+    }
+    private func MusicTest() {
+        var index = 1
+        Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { timer in
+            index += 1
+            if index == 2000 {
+                timer.invalidate()
+            }
+            
+            let musicItemID = MusicItemID(rawValue: String(index))
+            let request = MusicCatalogResourceRequest<Genre>(matching: \.id, equalTo: musicItemID)
+            Task{
+                let response = try await request.response()
+                if let genre = response.items.first {
+                    print("genre id: \(genre.id), name: \(genre.name)")
+                }else {
+                    print("no genre")
+                }
+            }
+            
+        }
     }
 }
 
