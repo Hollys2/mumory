bf4c45f188169526e637354d4b1d8a36f10fea71
kane04036 <hi011004@naver.com>
2024-03-13 21:38:36 +0900

    add notify view

---
+++ Projects/Feature/SocialMypage/Sources/View/ActivityListView.swift
@@ -0,0 +1,19 @@
+//
+//  ActivityListView.swift
+//  Feature
+//
+//  Created by 제이콥 on 3/11/24.
+//  Copyright © 2024 hollys. All rights reserved.
+//
+
+import SwiftUI
+
+struct ActivityListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ActivityListView()
+}
---
+++ Projects/Feature/SocialMypage/Sources/View/PagingTestView.swift
@@ -0,0 +1,19 @@
+//
+//  PagingTestView.swift
+//  Feature
+//
+//  Created by 제이콥 on 3/13/24.
+//  Copyright © 2024 hollys. All rights reserved.
+//
+
+import SwiftUI
+
+struct PagingTestView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PagingTestView()
+}
