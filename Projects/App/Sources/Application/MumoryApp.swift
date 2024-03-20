e1ece665f79d39254cfb45738297ee55a1ffc4ef
2o1da <2o1daaaaaaa@gmail.com>
2024-03-20 16:58:48 +0900

    refactoring

--- Projects/App/Sources/Application/MumoryApp.swift
+++ Projects/App/Sources/Application/MumoryApp.swift
@@ -24,7 +24,8 @@ struct MumoryApp: App {
         WindowGroup {
             GeometryReader { geometry in
                 ZStack {
-                    SplashView()
+//                    SplashView()
+                    MumoryEditView(mumoryAnnotation: Mumory())
                         .onOpenURL(perform: { url in
                             if (AuthApi.isKakaoTalkLoginUrl(url)) {
                                 AuthController.handleOpenUrl(url: url)
