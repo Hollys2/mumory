69b2a15b77bf9a1bda4c0d6b83aea750566f699b
kane04036 <hi011004@naver.com>
2024-03-31 00:58:11 +0900

    add code - kane

--- Projects/Feature/Library/Sources/View/NowPlayingView.swift
+++ Projects/Feature/Library/Sources/View/NowPlayingView.swift
@@ -68,6 +68,7 @@ struct NowPlayingView: View {
 //                            }
 //                        })
                 }
+                .scrollIndicators(.hidden)
                 .scrollDisabled(isPresentQueue)
                 .onChange(of: isPresentQueue) { newValue in
                     if isPresentQueue{
@@ -629,6 +630,7 @@ private func getTextWidth(term: String) -> CGFloat {
 
 
 struct PlayTogetherView: View {
+    @EnvironmentObject var playerViewModel: PlayerViewModel
     @Binding var songs: [Song]
     init(songs: Binding<[Song]>) {
         self._songs = songs
@@ -643,12 +645,24 @@ struct PlayTogetherView: View {
                 .padding(.leading, 15)
                 .padding(.bottom, 12)
             
-            VStack(spacing: 0) {
-                ForEach(songs, id: \.id) { song in
-                    PlayTogetherItem(song: song)
+            if let currentSong = playerViewModel.currentSong {
+                VStack(spacing: 0) {
+                    ForEach(songs, id: \.id) { song in
+                        PlayTogetherItem(song: song)
+                            .onTapGesture {
+                                playerViewModel.playNewSong(song: song)
+                            }
+                    }
                 }
+                .padding(.bottom, 25)
+            }else {
+                Text("재생중인 음악이 없습니다.")
+                    .foregroundStyle(ColorSet.subGray)
+                    .font(SharedFontFamily.Pretendard.medium.swiftUIFont(size: 16))
+                    .multilineTextAlignment(.center)
+                    .padding(.bottom, 30)
+                    .frame(height: 280)
             }
-            .padding(.bottom, 25)
             
         }
         .frame(width: getUIScreenBounds().width * 0.92)
